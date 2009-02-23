3ddab4d424c31ee4bceacb88b8fccd7ccf8d5827
Joaquin Bravo Contreras <jackbravo@gmail.com>
2009-02-23 12:56:09 -0600

    Configurar filtro de encuestas

--- lib/filter/doctrine/EncuestaFormFilter.class.php
+++ lib/filter/doctrine/EncuestaFormFilter.class.php
@@ -11,5 +11,13 @@ class EncuestaFormFilter extends BaseEncuestaFormFilter
 {
   public function configure()
   {
+    $this->widgetSchema['nombre']->setOption('with_empty', false);
+    $this->widgetSchema['apellido_p']->setOption('with_empty', false);
+    $this->widgetSchema['estado_id']->setOption('add_empty', 'cualquier estado');
+
+    $this->widgetSchema->setLabels(array(
+      'apellido_p' => 'Apellido Paterno',
+      'estado_id' => 'en',
+    ));
   }
 }
