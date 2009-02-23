390f52264db3b2d96d090eaa783a8b5a32d2b20e
Joaquin Bravo Contreras <jackbravo@gmail.com>
2009-02-23 12:00:12 -0600

    Poner al encuestador del sfContext antes de insertar una nueva encuesta

--- lib/model/doctrine/Encuesta.class.php
+++ lib/model/doctrine/Encuesta.class.php
@@ -5,5 +5,13 @@
  */
 class Encuesta extends BaseEncuesta
 {
-
+  public function preInsert($event)
+  {
+    $modified = $this->getModified();
+    if (!array_key_exists('encuestador_id', $modified))
+    {
+      $this->encuestador_id = sfContext::getInstance()->getUser()
+        ->getAttribute('user_id', null, 'sfGuardSecurityUser');
+    }
+  }
 }
