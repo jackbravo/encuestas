f2d4237cfaced50df26d4930cbace5bc66407368
Joaquin Bravo Contreras <jackbravo@gmail.com>
2009-02-23 11:02:16 -0600

    Cambiar la redaccion de las preguntas

--- lib/form/doctrine/EncuestaForm.class.php
+++ lib/form/doctrine/EncuestaForm.class.php
@@ -12,5 +12,13 @@ class EncuestaForm extends BaseEncuestaForm
   public function configure()
   {
     unset($this['encuestador_id'], $this['created_at'], $this['updated_at']);
+
+    $this->widgetSchema->setLabels(array(
+      'estado_id' => 'Estado',
+      'horarios_list' => 'Mejor horario para contactarle',
+      'areas_interes_list' => '¿A usted le gustaría..?',
+      'productos_interes_list' => '¿En qué productos está interesado?',
+      'medios_contacto_list' => '¿Cómo se enteró de este número?',
+    ));
   }
 }
