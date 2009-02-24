137cf1bc02bffb43774908cc7887af74855d51f0
Joaquin Bravo Contreras <jackbravo@gmail.com>
2009-02-23 19:31:11 -0600

    Usar HydrateArray en encuestas

--- apps/front/modules/encuesta/templates/indexSuccess.php
+++ apps/front/modules/encuesta/templates/indexSuccess.php
@@ -26,19 +26,19 @@
     </tr>
   </thead>
   <tbody>
-    <?php foreach ($pager->getResults() as $i => $encuesta): ?>
+    <?php foreach ($pager->getResults('array') as $i => $encuesta): ?>
     <tr class="<?php
         echo fmod($i,2) == 0 ? 'even' : 'odd';
       ?>">
-      <td><?php echo link_to($encuesta->id, 'encuesta_show', $encuesta) ?></td>
-      <td><?php echo link_to($encuesta->nombre . ' ' . $encuesta->apellido_p . ' ' . $encuesta->apellido_m, 'encuesta_show', $encuesta) ?></td>
-      <td><?php echo $encuesta->telefono ?></td>
-      <td><?php echo $encuesta->Estado ?></td>
-      <td><?php echo $encuesta->Encuestador ?></td>
-      <td><?php echo $encuesta->updated_at ?></td>
+      <td><?php echo link_to($encuesta['id'], '@encuesta_show?id=' . $encuesta['id']) ?></td>
+      <td><?php echo link_to($encuesta['nombre'] . ' ' . $encuesta['apellido_p'] . ' ' . $encuesta['apellido_m'], '@encuesta_show?id=' . $encuesta['id']) ?></td>
+      <td><?php echo $encuesta['telefono'] ?></td>
+      <td><?php echo $encuesta['Estado']['nombre'] ?></td>
+      <td><?php echo $encuesta['Encuestador']['username'] ?></td>
+      <td><?php echo $encuesta['updated_at'] ?></td>
     </tr>
     <?php endforeach; ?>
   </tbody>
 </table>
 
-  <a href="<?php echo url_for('encuesta_new') ?>">New</a>
+<a href="<?php echo url_for('encuesta_new') ?>">Nueva encuesta</a>
