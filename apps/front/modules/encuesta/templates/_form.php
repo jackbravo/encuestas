4d0f0f94cdfc6538d9fe355844bc9efff4a58c49
Joaquin Bravo Contreras <jackbravo@gmail.com>
2009-02-25 00:07:51 -0600

    Hacer las formas mas sencillas y presentables (#141)

--- apps/front/modules/encuesta/templates/_form.php
+++ apps/front/modules/encuesta/templates/_form.php
@@ -5,40 +5,80 @@
   <?php echo $form->renderHiddenFields() ?>
   <?php echo $form->renderGlobalErrors() ?>
 
+<h4>Información Personal</h4>
 <fieldset>
-  <legend>Datos</legend>
+  <div class="grid_4">
   <?php echo $form['nombre']->renderRow() ?>
+  </div>
+  <div class="grid_4">
   <?php echo $form['apellido_p']->renderRow() ?>
+  </div>
+  <div class="grid_4">
   <?php echo $form['apellido_m']->renderRow() ?>
+  </div>
+
+  <div class="grid_4">
   <?php echo $form['rfc']->renderRow() ?>
+  </div>
+  <div class="grid_8">
   <?php echo $form['edad']->renderRow() ?>
+  </div>
+  <div class="grid_12">
   <?php echo $form['genero']->renderRow() ?>
+  </div>
 </fieldset>
 
+<h4>Dirección</h4>
 <fieldset>
-  <legend>Dirección</legend>
+  <div class="grid_4">
   <?php echo $form['calle']->renderRow() ?>
+  </div>
+  <div class="grid_4">
   <?php echo $form['numero']->renderRow() ?>
+  </div>
+  <div class="grid_4">
   <?php echo $form['colonia']->renderRow() ?>
+  </div>
+
+  <div class="grid_4">
   <?php echo $form['ciudad']->renderRow() ?>
+  </div>
+  <div class="grid_4">
   <?php echo $form['municipio']->renderRow() ?>
+  </div>
+  <div class="grid_4">
   <?php echo $form['estado_id']->renderRow() ?>
+  </div>
+
+  <div class="grid_12">
   <?php echo $form['cp']->renderRow() ?>
+  </div>
 </fieldset>
 
+<h4>Contacto</h4>
 <fieldset>
-  <legend>Contacto</legend>
+  <div class="grid_4">
   <?php echo $form['telefono']->renderRow() ?>
+  </div>
+  <div class="grid_4">
   <?php echo $form['celular']->renderRow() ?>
+  </div>
+  <div class="grid_4">
   <?php echo $form['email']->renderRow() ?>
+  </div>
+
+  <div class="grid_12">
   <?php echo $form['horarios_list']->renderRow() ?>
+  </div>
 </fieldset>
 
+<h4>Interés</h4>
 <fieldset>
-  <legend>Interés</legend>
+  <div class="grid_12">
   <?php echo $form['areas_interes_list']->renderRow() ?>
   <?php echo $form['productos_interes_list']->renderRow() ?>
   <?php echo $form['medios_contacto_list']->renderRow() ?>
+  </div>
 </fieldset>
 
   &nbsp;<a href="<?php echo url_for('encuesta') ?>">Cancel</a>
