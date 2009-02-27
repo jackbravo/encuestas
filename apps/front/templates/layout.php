f82c22c81910a9b16c61a63db04481d4e9c10670
Joaquin Bravo Contreras <jackbravo@gmail.com>
2009-02-27 14:17:45 -0600

    Activar I18N helper para todos los templates

--- apps/front/templates/layout.php
+++ apps/front/templates/layout.php
@@ -65,13 +65,13 @@ $('#ajax-loader').ajaxStart(function(){
 
   <?php if ($sf_user->hasFlash('notice')): ?>
     <div class="box notice">
-      <?php echo $sf_user->getFlash('notice') ?>
+      <?php echo __($sf_user->getFlash('notice')) ?>
     </div>
   <?php endif; ?>
 
   <?php if ($sf_user->hasFlash('error')): ?>
     <div class="box error">
-      <?php echo $sf_user->getFlash('error') ?>
+      <?php echo __($sf_user->getFlash('error')) ?>
     </div>
   <?php endif; ?>
 
