506abc8e06eef0c3d8ef08f58cb34d41c8dcad3c
Joaquin Bravo Contreras <jackbravo@gmail.com>
2009-02-28 11:26:12 -0600

    Listar los seguimientos en show encuesta

--- apps/front/modules/encuesta/templates/showSuccess.php
+++ apps/front/modules/encuesta/templates/showSuccess.php
@@ -1,3 +1,4 @@
+<?php use_helper('Text') ?>
 <div id="content" class="grid_8">
 
   <div class="subheader">
@@ -25,8 +26,29 @@
     <li><?php echo $producto ?></li>
   <?php endforeach; ?>
   </ul>
-
+  
   <hr />
+
+  <div class="historial">
+  <?php foreach ($encuesta->Seguimiento as $seguimiento): ?>
+    <div class="seguimiento">
+      <p class="seguimiento-timestamp">
+        <strong><?php echo $seguimiento->Agente ?></strong> asignó a
+        <strong><?php echo $seguimiento->Distribuidor ?></strong>
+        <small><?php echo $seguimiento->created_at ?></small>
+      </p>
+      <?php echo simple_format_text($seguimiento->notas) ?>
+    </div>
+  <?php endforeach ?>
+  </div>
+
+  <div class="actions">
+  <?php if ($encuesta->viewer_id != $sf_user->getId()): ?>
+    Este encuestado está siendo editado por <?php $encuesta->Viewer->username ?>
+  <?php else: ?>
+    HOla
+  <?php endif; ?>
+  </div>
 
 </div> <!-- /grid_8 -->
 
