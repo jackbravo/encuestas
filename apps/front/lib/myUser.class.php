9353e9b79a12053291c73ec4468ca1c038f247f1
Joaquin Bravo Contreras <jackbravo@gmail.com>
2009-02-28 20:17:01 -0600

    Metodos de conveniencia para user

--- apps/front/lib/myUser.class.php
+++ apps/front/lib/myUser.class.php
@@ -6,4 +6,23 @@ class myUser extends sfGuardSecurityUser
   {
     return $this->getAttribute('user_id', null, 'sfGuardSecurityUser');
   }
+
+  public static function getCurrentId()
+  {
+    try
+    {
+      return sfContext::getInstance()->getUser()
+        ->getAttribute('user_id', null, 'sfGuardSecurityUser');
+    }
+    catch (Exception $e)
+    {
+      return null;
+    }
+  }
+
+  public function signOut()
+  {
+    Doctrine::getTable('Encuesta')->unlockAll($this->getId());
+    parent::signOut();
+  }
 }
