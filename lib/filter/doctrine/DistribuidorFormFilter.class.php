60690852384b2e82db8529cc9b943343091b5dcf
Joaquin Bravo Contreras <jackbravo@gmail.com>
2009-02-28 20:16:35 -0600

    Hacer los filtros bonitos para distribuidor

--- lib/filter/doctrine/DistribuidorFormFilter.class.php
+++ lib/filter/doctrine/DistribuidorFormFilter.class.php
@@ -11,5 +11,14 @@ class DistribuidorFormFilter extends BaseDistribuidorFormFilter
 {
   public function configure()
   {
+    $this->widgetSchema['city']->setOption('with_empty', false);
+    $this->widgetSchema['city']->setOption('template', '%input%');
+    $this->widgetSchema['state']->setOption('with_empty', false);
+    $this->widgetSchema['state']->setOption('template', '%input%');
+
+    $this->widgetSchema->setLabels(array(
+      'city' => 'Ciudad',
+      'state' => 'Estado',
+    ));
   }
 }
