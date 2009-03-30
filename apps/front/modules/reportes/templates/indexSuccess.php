27bab57c15b057a8748ef60d7a2763561911dcc3
Joaquin Bravo Contreras <jackbravo@gmail.com>
2009-03-30 14:06:23 -0600

    Preparar para agregar reportes de tabs

--- apps/front/modules/reportes/templates/indexSuccess.php
+++ apps/front/modules/reportes/templates/indexSuccess.php
@@ -1,4 +1,11 @@
-<h1>Reportes</h1>
+<h1>Reportes de Agentes</h1>
+
+<div class="tabs">
+  <?php echo link_to('Agentes', 'reportes/index') ?>
+  &nbsp;<?php echo link_to('TAB Team', 'reportes/tabs') ?>
+</div>
+
+<br/>
 
 <form method="get">
 <?php echo $filter ?>
