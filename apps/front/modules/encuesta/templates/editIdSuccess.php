1e6099b5715f286fcf69c69abcbf936a07c96558
unknown <CesarRo@.hrbl.net>
2009-04-01 17:19:26 -0600

    Cambios porque el url_for y link_to no estan jalando bien en windows en produccion

--- apps/front/modules/encuesta/templates/editIdSuccess.php
+++ apps/front/modules/encuesta/templates/editIdSuccess.php
@@ -1,6 +1,6 @@
 <h1>Asignar ID a <?php echo $form->getObject() ?></h1>
 
-<form action="<?php echo url_for('encuesta_updateId', $form->getObject()) ?>" method="post">
+<form action="<?php echo url_for('@encuesta_updateId?id=' . $form->getObject()->id) ?>" method="post">
   <?php echo $form ?>
   <input type="submit" value="Enviar" />
 </form>
