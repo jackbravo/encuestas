892636e3f04be8350b5f77d12f3fe60b00a84899
unknown <CesarRo@.hrbl.net>
2009-04-04 13:04:03 -0600

    Para evitar un bug de symfony

--- web/index.php
+++ web/index.php
@@ -3,5 +3,5 @@
 
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('front', 'prod', true);
+$configuration = ProjectConfiguration::getApplicationConfiguration('front', 'prod', false);
 sfContext::createInstance($configuration)->dispatch();
