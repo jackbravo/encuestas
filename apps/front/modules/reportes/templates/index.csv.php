0c189f8daaedf326209ff70dae47ecbbb192c19d
Joaquin Bravo Contreras <jackbravo@gmail.com>
2009-04-09 23:17:51 -0500

    Acomodar mejor el reporte de leads, igual que el de tabs, para facilitar su entendimiento

--- apps/front/modules/reportes/templates/index.csv.php
+++ apps/front/modules/reportes/templates/index.csv.php
@@ -1,12 +1,15 @@
 <?php
 echo '"Agente",' .
-  '"Leads por agente",' .
-  '"Asignaciones de TAB por agente (48 hrs.)",' .
-  '"24 hrs.",' .
-  '"Asignaciones exitosas de TAB por agente (48 hrs.)",' .
-  '"24 hrs.",' .
-  '"Seguimientos a lead por agente (48 hrs.)",' .
-  '"24 hrs.",' .
+  '"Leads registrados",' .
+  '"Asignaciones de TAB 1a vuelta",' .
+  '"Asignaciones de TAB 2a vuelta",' .
+  '"Asignaciones de TAB (total)",' .
+  '"Asignaciones exitosas de TAB 1a vuelta",' .
+  '"Asignaciones exitosas de TAB 2a vuelta",' .
+  '"Asignaciones exitosas de TAB (total)",' .
+  '"Monitoreo de eguimientos 1a vuelta",' .
+  '"Monitoreo de eguimientos 2a vuelta",' .
+  '"Monitoreo de eguimientos (total)",' .
   '"IDs de distribuidor asignados"' .
   "\n";
 foreach($agents as $id => $agent)
@@ -18,13 +21,19 @@ foreach($agents as $id => $agent)
   echo ',';
   if (isset($tab_tries_per_agent_2[$agent])) echo $tab_tries_per_agent_2[$agent];
   echo ',';
+  echo (isset($tab_tries_per_agent[$agent])? $tab_tries_per_agent[$agent] : 0) + (isset($tab_tries_per_agent_2[$agent])? $tab_tries_per_agent_2[$agent] : 0);
+  echo ',';
   if (isset($tab_per_agent[$agent])) echo $tab_per_agent[$agent];
   echo ',';
   if (isset($tab_per_agent_2[$agent])) echo $tab_per_agent_2[$agent];
   echo ',';
+  echo (isset($tab_per_agent[$agent])? $tab_per_agent[$agent] : 0) + (isset($tab_per_agent_2[$agent])? $tab_per_agent_2[$agent] : 0);
+  echo ',';
   if (isset($lead_per_agent[$agent])) echo $lead_per_agent[$agent];
   echo ',';
   if (isset($lead_per_agent_2[$agent])) echo $lead_per_agent_2[$agent];
+  echo ',';
+  echo (isset($lead_per_agent[$agent])? $lead_per_agent[$agent] : 0) + (isset($lead_per_agent_2[$agent])? $lead_per_agent_2[$agent] : 0);
   echo ',';
   if (isset($leads_to_dist[$agent])) echo $leads_to_dist[$agent];
   echo "\n";
--- apps/front/modules/reportes/templates/indexSuccess.php
+++ apps/front/modules/reportes/templates/indexSuccess.php
@@ -16,42 +16,43 @@
 <table class="list table">
   <thead>
     <tr>
+      <th></th>
+      <th></th>
+      <th colspan="3">Asignaciones de TAB</th>
+      <th colspan="3">Asignaciones exitosas de TAB</th>
+      <th colspan="3">Seguimientos<br><small>Monitoreo de seguimientos a leads</small></th>
+      <th></th>
+    </tr>
+    <tr>
       <th>Agente</th>
-      <th>Leads por agente</th>
-      <th>Asignaciones de TAB por agente (48 hrs.)</th>
-      <th>24 hrs.</th>
-      <th>Asignaciones exitosas de TAB por agente (48 hrs.)</th>
-      <th>24 hrs.</th>
-      <th>Seguimientos a lead por agente (48 hrs.)</th>
-      <th>24 hrs.</th>
+      <th>Leads registrados</th>
+      <th>1a vuelta</th>
+      <th>2a vuelta</th>
+      <th>Total</th>
+      <th>1a vuelta</th>
+      <th>2a vuelta</th>
+      <th>Total</th>
+      <th>1a vuelta</th>
+      <th>2a vuelta</th>
+      <th>Total</th>
       <th>IDs de distribuidor asignados</th>
     </tr>
   </thead>
   <tbody>
     <?php foreach($agents as $id => $agent): ?>
-      <?php if (fmod($id, 15) == 0): ?>
-        <tr>
-          <th>Agente</th>
-          <th>Leads por agente</th>
-          <th>Asignaciones de TAB por agente (48 hrs.)</th>
-          <th>24 hrs.</th>
-          <th>Asignaciones exitosas de TAB por agente (48 hrs.)</th>
-          <th>24 hrs.</th>
-          <th>Seguimientos a lead por agente (48 hrs.)</th>
-          <th>24 hrs.</th>
-          <th>IDs de distribuidor asignados</th>
-        </tr>
-      <?php endif ?>
       <tr>
         <td><?php echo $agent ?></td>
-        <td title="Leads por agente"><?php if (isset($leads_per_agent[$agent])) echo $leads_per_agent[$agent] ?></td>
-        <td title="Asignaciones de TAB por agente (48 hrs.)"><?php if (isset($tab_tries_per_agent[$agent])) echo $tab_tries_per_agent[$agent] ?></td>
-        <td title="Asignaciones de TAB por agente (24 hrs.)"><?php if (isset($tab_tries_per_agent_2[$agent])) echo $tab_tries_per_agent_2[$agent] ?></td>
-        <td title="Asignaciones exitosas de TAB por agente (48 hrs.)"><?php if (isset($tab_per_agent[$agent])) echo $tab_per_agent[$agent] ?></td>
-        <td title="Asignaciones exitosas de TAB por agente (24 hrs.)"><?php if (isset($tab_per_agent_2[$agent])) echo $tab_per_agent_2[$agent] ?></td>
-        <td title="Seguimientos a leads por agente (48 hrs.)"><?php if (isset($lead_per_agent[$agent])) echo $lead_per_agent[$agent] ?></td>
-        <td title="Seguimientos a leads por agente (24 hrs.)"><?php if (isset($lead_per_agent_2[$agent])) echo $lead_per_agent_2[$agent] ?></td>
-        <td title="IDs de distribuidor ssignados"><?php if (isset($leads_to_dist[$agent])) echo $leads_to_dist[$agent] ?></td>
+        <td><?php if (isset($leads_per_agent[$agent])) echo $leads_per_agent[$agent] ?></td>
+        <td><?php if (isset($tab_tries_per_agent[$agent])) echo $tab_tries_per_agent[$agent] ?></td>
+        <td><?php if (isset($tab_tries_per_agent_2[$agent])) echo $tab_tries_per_agent_2[$agent] ?></td>
+        <td><?php echo (isset($tab_tries_per_agent[$agent])? $tab_tries_per_agent[$agent] : 0) + (isset($tab_tries_per_agent_2[$agent])? $tab_tries_per_agent_2[$agent] : 0) ?></td>
+        <td><?php if (isset($tab_per_agent[$agent])) echo $tab_per_agent[$agent] ?></td>
+        <td><?php if (isset($tab_per_agent_2[$agent])) echo $tab_per_agent_2[$agent] ?></td>
+        <td><?php echo (isset($tab_per_agent[$agent])? $tab_per_agent[$agent] : 0) + (isset($tab_per_agent_2[$agent])? $tab_per_agent_2[$agent] : 0) ?></td>
+        <td><?php if (isset($lead_per_agent[$agent])) echo $lead_per_agent[$agent] ?></td>
+        <td><?php if (isset($lead_per_agent_2[$agent])) echo $lead_per_agent_2[$agent] ?></td>
+        <td><?php echo (isset($lead_per_agent[$agent])? $lead_per_agent[$agent] : 0) + (isset($lead_per_agent_2[$agent])? $lead_per_agent_2[$agent] : 0) ?></td>
+        <td><?php if (isset($leads_to_dist[$agent])) echo $leads_to_dist[$agent] ?></td>
       </tr>
     <?php endforeach ?>
   </tbody>
