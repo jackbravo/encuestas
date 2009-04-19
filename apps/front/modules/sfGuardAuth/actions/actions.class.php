8855eca477626b834b755b90c629d17f52c3ef84
Joaquin Bravo Contreras <jackbravo@gmail.com>
2009-04-19 15:08:13 -0500

    require once porque no encontraba la clase en linux

--- apps/front/modules/sfGuardAuth/actions/actions.class.php
+++ apps/front/modules/sfGuardAuth/actions/actions.class.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once(sfConfig::get('sf_plugins_dir').'/sfDoctrineGuardPlugin/modules/sfGuardAuth/lib/BasesfGuardAuthActions.class.php');
+
 class sfGuardAuthActions extends BasesfGuardAuthActions
 {
   public function executeSignin($request)
