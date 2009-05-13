227d62f5f61c6606e3448585567bd86a2903443a
Joaquin Bravo Contreras <jackbravo@gmail.com>
2009-05-12 20:27:02 -0500

    Agregar menu de navegacion de reportes al reporte de exportacion de tablas

--- apps/front/modules/reportes/templates/exportarSuccess.php
+++ apps/front/modules/reportes/templates/exportarSuccess.php
@@ -1,5 +1,9 @@
 <h1>Exportar Tablas</h1>
 
+<?php include_partial('nav') ?>
+
+<br/>
+
 <ul>
   <li><?php echo link_to('Exportar reporte global', 'reportes/global') ?></li>
   <li><?php echo link_to('Exportar tabla de asignaciones', 'reportes/seguimientos') ?></li>
